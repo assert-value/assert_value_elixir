3f23d621f44450585c08becd002abd87196a1e4e
Serge Smetana <smetana@pluron.com>
2015-12-17 17:58:53 +0200

    Implemented AssertValue.text_diff This function takes strings and char lists  (doc string) as argument and returns string formatted as unified diff. For now it returns full string with changes (not chunks)

--- lib/assert_value.ex
+++ lib/assert_value.ex
@@ -1,2 +1,31 @@
 defmodule AssertValue do
+
+  @prefixes %{eq: " ", ins: "+", del: "-"}
+
+  def text_diff(a, b) do
+    a = to_string_list a
+    b = to_string_list b
+    format_diff :tdiff.diff(a, b)
+  end
+
+  defp to_string_list(string_or_char_list) do
+    string_or_char_list
+    |> to_string
+    |> String.rstrip(?\n)
+    |> String.split("\n")
+  end
+
+  defp format_diff(diff) do
+    formatted =
+      diff
+      |> Enum.map(fn({k,v}) ->
+          Enum.map(v, fn(s) ->
+            @prefixes[k] <> s
+          end)
+        end)
+      |> List.flatten
+      |> Enum.join("\n")
+    formatted <> "\n"
+  end
+
 end
--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -2,7 +2,49 @@ defmodule AssertValueTest do
   use ExUnit.Case
   doctest AssertValue
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "text_diff char lists" do
+    a = '''
+    aaa
+    bbb
+    ccc
+    ddd
+    eee
+    fff
+    '''
+    b = '''
+    aaa
+    xxx
+    ddd
+    eee
+    fff
+    GGG
+    '''
+    assert AssertValue.text_diff(a, b) == to_string '''
+     aaa
+    -bbb
+    -ccc
+    +xxx
+     ddd
+     eee
+     fff
+    +GGG
+    '''
   end
+
+  test "text_diff strings" do
+    a = "aaa\nbbb\nccc\nddd\neee\nfff\n"
+    b = "aaa\nxxx\nddd\neee\nfff\nGGG\n"
+    assert AssertValue.text_diff(a, b) == to_string '''
+     aaa
+    -bbb
+    -ccc
+    +xxx
+     ddd
+     eee
+     fff
+    +GGG
+    '''
+  end
+
+
 end
