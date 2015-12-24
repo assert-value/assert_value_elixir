081a459079ef29361b6c00de14ab856dc50de0aa
Serge Smetana <smetana@pluron.com>
2015-12-24 19:08:04 +0200

    Simple test to run external test:  - run test case producing diff prompt  - say "n\n"  - check output

---
+++ test/simple_diff_test.exs
@@ -0,0 +1,38 @@
+defmodule SimpleDiffTest do
+  use ExUnit.Case
+
+  import AssertValue
+
+  import AssertValue.TestHelpers
+
+  test "simple diff test" do
+    test_case = prepare_test_case "simple_diff_test.exs"
+    {result, exitcode} = run_test_case(test_case, "n\n")
+    assert exitcode == 1
+    assert_value result == ~S"""
+    
+    <Failed Assertion Message>
+        actual == "aaa\nbBb\nccc\n"
+    
+     aaa
+    -bBb
+    +bbb
+     ccc
+    
+    Accept new value [y/n]? 
+    
+      1) test simple (SimpleDiffTest)
+         /tmp/simple_diff_test.exs:6
+         AssertValue assertion failed
+         code: actual == "aaa\nbBb\nccc\n"
+         lhs:  "aaa\nbbb\nccc\n"
+         rhs:  "aaa\nbBb\nccc\n"
+         stacktrace:
+           /tmp/simple_diff_test.exs:12
+    
+    
+    1 test, 1 failure
+    """
+  end
+
+end
