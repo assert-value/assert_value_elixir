26198064da1d0cc59e087732e9855f4bc5cac69f
Serge Smetana <smetana@pluron.com>
2015-12-24 19:03:29 +0200

    Added Porcelain as a dependency. https://github.com/alco/porcelain Porcelain is a library to run and communicate with OS processes

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule AssertValue.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [mod: {AssertValue.FileOffsets.App, []}]
+    [
+      applications: [:porcelain],
+      mod: {AssertValue.FileOffsets.App, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +30,6 @@ defmodule AssertValue.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:porcelain, "~> 2.0"}]
   end
 end
