369175bbb89d01185a35aeb0e37e6a139a94828a
Serge Smetana <smetana@pluron.com>
2015-12-24 17:36:07 +0200

    AssertValue test prototype: Able to copy test source to working directory, execute test and get the output

---
+++ test/simple_test.exs
@@ -0,0 +1,16 @@
+defmodule SimpleTest do
+  use ExUnit.Case
+
+  import AssertValue.TestHelpers
+
+  test "simple pass test" do
+    test_case = prepare_test_case "simple_pass_test.exs"
+    {result, exitcode} = run_test(test_case)
+    assert exitcode == 0
+    assert result == """
+    .
+    1 test, 0 failures
+    """
+  end
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,25 @@
 ExUnit.start()
+
+defmodule AssertValue.TestHelpers do
+
+  @source_dir Path.expand("tests_src", __DIR__)
+  @target_dir Path.expand("tmp", __DIR__)
+
+  def prepare_test_case(test_file) do
+    source_filename = Path.expand(test_file <> ".src", @source_dir)
+    target_filename = Path.expand(test_file, @target_dir)
+    File.cp!(source_filename, target_filename)
+    target_filename
+  end
+
+  def run_test(test_case_file) do
+    {output, exitcode} = System.cmd "mix", ["test", test_case_file]
+    # Serialize output
+    output =
+      output
+      |> String.replace(~r/\nFinished in.*\n/m, "")
+      |> String.replace(~r/\nRandomized with seed.*\n/m, "")
+    {output, exitcode}
+  end
+
+end
