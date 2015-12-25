21bcd549acfa3bda3c337d87942941a16b15305c
Serge Smetana <smetana@pluron.com>
2015-12-25 17:34:44 +0200

    Since we use AssertValue.Tmpname only in tests there is no need to keep in as a part of AssertValue. Moved it to test/test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,21 @@
 ExUnit.start()
 
+defmodule AssertValue.Tmpname do
+  @moduledoc """
+  This module ispired by ruby's tmpname (ruby/lib/tmpdir.rb)
+  and elixir Plug's upload.ex
+  """
+  def generate(prefix \\ "", suffix \\ "") do
+    {_mega, sec, micro} = :os.timestamp
+    pid = :os.getpid
+    random_string = Integer.to_string(:rand.uniform(0x100000000), 36) |> String.downcase
+    "#{prefix}#{sec}#{micro}-#{pid}-#{random_string}#{suffix}"
+  end
+end
+
 defmodule AssertValue.TestHelpers do
+
+  require AssertValue.Tmpname
 
   @source_dir Path.expand("tests_src", __DIR__)
   @target_dir Path.expand(AssertValue.Tmpname.generate("assert-value-"), System.tmp_dir!)
