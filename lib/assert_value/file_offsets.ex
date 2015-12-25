f5cf037bac8130c43ed2a4bcd1038ba65ac9f353
Serge Smetana <smetana@pluron.com>
2015-12-25 19:19:17 +0200

    Do not restart AssertValue.FileOffsets worker

--- lib/assert_value/file_offsets.ex
+++ lib/assert_value/file_offsets.ex
@@ -5,7 +5,7 @@ defmodule AssertValue.FileOffsets do
     def start(_type, _args) do
       import Supervisor.Spec, warn: false
       children = [
-        worker(AssertValue.FileOffsets, [%{}])
+        worker(AssertValue.FileOffsets, [%{}], restart: :temporary)
       ]
       opts = [strategy: :one_for_one]
       Supervisor.start_link(children, opts)
