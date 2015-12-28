fa6a8cb587fa070385fc6edea95bbcf59c703331
Serge Smetana <smetana@pluron.com>
2015-12-28 18:27:09 +0200

    Moved all small tests to one AssertValueTest testcase

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -1,8 +1,15 @@
-defmodule SimpleDiffTest do
+defmodule SimpleTest do
   use ExUnit.Case
 
   import AssertValue
   import AssertValue.TestHelpers
+
+  test "simple pass test" do
+    {result, exitcode, log_filename} =
+      prepare_and_run_test_case("simple_pass_test.exs")
+    assert exitcode == 0
+    assert_value result == File.read!(log_filename)
+  end
 
   test "simple diff test" do
     {result, exitcode, log_filename} =
