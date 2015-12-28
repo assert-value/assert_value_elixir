8d85eab2f67fa62ab1c9f1426e22a53fd48cae91
Serge Smetana <smetana@pluron.com>
2015-12-28 14:48:37 +0200

    Renamed AsserValue.Tmpname to AssertValue.Tempfile. Added function mktemp_dir to this new module. Used this function in test helpers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,15 @@
 ExUnit.start()
 
-defmodule AssertValue.Tmpname do
-  @moduledoc """
-  This module ispired by ruby's tmpname (ruby/lib/tmpdir.rb)
-  and elixir Plug's upload.ex
-  """
-  def generate(prefix \\ "", suffix \\ "") do
+defmodule AssertValue.Tempfile do
+  def mktemp_dir(prefix \\ "", suffix \\ "") do
+    name = Path.expand(generate_tmpname(prefix, suffix), System.tmp_dir!)
+    File.mkdir_p!(name)
+    name
+  end
+
+  # This function ispired by ruby's tmpname (ruby/lib/tmpdir.rb)
+  # and elixir Plug's upload.ex
+  defp generate_tmpname(prefix, suffix) do
     {_mega, sec, micro} = :os.timestamp
     pid = :os.getpid
     random_string = Integer.to_string(:rand.uniform(0x100000000), 36) |> String.downcase
@@ -15,15 +19,14 @@ end
 
 defmodule AssertValue.TestHelpers do
 
-  require AssertValue.Tmpname
+  require AssertValue.Tempfile
 
   @source_dir Path.expand("tests_src", __DIR__)
-  @target_dir Path.expand(AssertValue.Tmpname.generate("assert-value-"), System.tmp_dir!)
+  @target_dir AssertValue.Tempfile.mktemp_dir("assert-value-")
 
   def prepare_test_case(test_file) do
     source_filename = Path.expand(test_file <> ".src", @source_dir)
     target_filename = Path.expand(test_file, @target_dir)
-    File.mkdir_p!(@target_dir)
     File.cp!(source_filename, target_filename)
     target_filename
   end
