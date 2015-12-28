7afb691e833e63c50703adcb72452398d634d2e9
Serge Smetana <smetana@pluron.com>
2015-12-28 19:12:01 +0200

    Added integration test for expected value create

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -30,4 +30,16 @@ defmodule AssertValueTest do
     assert_value AssertValue.Diff.diff(original_source, updated_source) == File.read!(diff_log)
   end
 
+  test "create expected" do
+    test_source_filename = "create_expected_test.exs"
+    {result, exitcode, log_filename} = prepare_and_run_test_case(test_source_filename, "y\n")
+    assert exitcode == 0
+    assert_value result == File.read!(log_filename)
+    {original_file, updated_file} = test_case_filenames(test_source_filename)
+    original_source = File.read!(original_file)
+    updated_source = File.read!(updated_file)
+    diff_log = "create_expected_test_diff.log" |> Path.expand(log_dir)
+    assert_value AssertValue.Diff.diff(original_source, updated_source) == File.read!(diff_log)
+  end
+
 end
