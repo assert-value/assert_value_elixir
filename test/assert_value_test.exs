6b75c8c895301369f44dae8a8343dac6d30c9e39
Serge Smetana <smetana@pluron.com>
2015-12-29 14:29:42 +0200

    Decomposed one integration test function to individual tests. It is too early on this stage to have them in one fuction. If one of the child test fail we don't know wich one because error report shows only parent test name and line number.

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -4,46 +4,60 @@ defmodule AssertValueTest do
   import AssertValue
   require AssertValue.Tempfile
 
-  # these are special integration tests. For each file we:
+  @integration_test_dir Path.expand("integration", __DIR__)
+  @runnable_test_dir AssertValue.Tempfile.mktemp_dir("assert-value-")
+
+  test "simple_pass_test.exs" do
+    run_integration_test(__ENV__, "", 0)
+  end
+
+  test "simple_diff_test.exs" do
+    run_integration_test(__ENV__, "n\n", 1)
+  end
+
+  test "update_expected_test.exs" do
+    run_integration_test(__ENV__, "y\n", 0)
+  end
+
+  test "create_expected_test.exs" do
+    run_integration_test(__ENV__, "y\n", 0)
+  end
+
+  # For each test we:
   # * launch a child `mix test our_integration_test.exs`
   # * accept or reject assert_value changes when prompted
   # * compare test source file itself after the run with a reference copy
   # * compare test output with a reference copy
-  test "integration" do
-    integration_test_dir = Path.expand("integration", __DIR__)
-    runnable_test_dir = AssertValue.Tempfile.mktemp_dir("assert-value-")
+  defp run_integration_test(env, responses, expected_exit_code) do
+    # __ENV__.function returns tuple {:"test simple_pass_test.exs", 1}
+    # where first argument is a generated test function name  as atom
+    # and second argument is a test function arity (1 by default)
+    {test_name, _} = env.function
+    basename = test_name |> to_string |> String.replace(~r/^test /, "")
 
-    [["simple_pass_test.exs", "", 0],
-     ["simple_diff_test.exs", "n\n", 1],
-     ["update_expected_test.exs", "y\n", 0],
-     ["create_expected_test.exs", "y\n", 0]] |>
-      Enum.each(fn([basename, responses, expected_exit_code]) ->
-        before_path = Path.expand(basename <> ".before", integration_test_dir)
-        after_path = Path.expand(basename <> ".after", integration_test_dir)
-        output_path = Path.expand(basename <> ".output", integration_test_dir)
+    before_path = Path.expand(basename <> ".before", @integration_test_dir)
+    after_path = Path.expand(basename <> ".after", @integration_test_dir)
+    output_path = Path.expand(basename <> ".output", @integration_test_dir)
 
-        # copy the test to a temp dir for running
-        runnable_path = Path.expand(basename, runnable_test_dir)
-        File.cp!(before_path, runnable_path)
+    # copy the test to a temp dir for running
+    runnable_path = Path.expand(basename, @runnable_test_dir)
+    File.cp!(before_path, runnable_path)
 
-        # run the test
-        %Porcelain.Result{out: output, status: exitcode} =
-          Porcelain.exec("mix", ["test", runnable_path],
-                         in: responses)
-        # canonicalize output
-        output =
-          output
-        |> String.replace(~r{\/tmp\/assert-value-\d+-\d+-\w+/}, "")
-        |> String.replace(~r/\nFinished in.*\n/m, "")
-        |> String.replace(~r/\nRandomized with seed.*\n/m, "")
+    # run the test
+    %Porcelain.Result{out: output, status: exitcode} =
+      Porcelain.exec("mix", ["test", runnable_path],
+                     in: responses)
+    # canonicalize output
+    output =
+      output
+    |> String.replace(~r{\/tmp\/assert-value-\d+-\d+-\w+/}, "")
+    |> String.replace(~r/\nFinished in.*\n/m, "")
+    |> String.replace(~r/\nRandomized with seed.*\n/m, "")
 
-        # compare the results
-        assert exitcode == expected_exit_code
-        assert_value output == File.read!(output_path)
-        assert_value File.read!(runnable_path) == File.read!(after_path)
-      end)
+    # compare the results
+    assert exitcode == expected_exit_code
+    assert_value output == File.read!(output_path)
+    assert_value File.read!(runnable_path) == File.read!(after_path)
   end
 
 end
-
-
