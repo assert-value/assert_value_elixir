98fab2c367136af47e1c4399239c45a10077a056
Gleb Arshinov <gleb-oss@barsook.com>
2015-12-29 00:23:28 -0500

    Reduce code duplication for integration tests

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -3,37 +3,27 @@ defmodule AssertValueTest do
 
   import AssertValue
   import AssertValue.TestHelpers
-
-  test "simple pass" do
-    {result, exitcode, log_filename} =
-      prepare_and_run_test_case("simple_pass_test.exs")
-    assert exitcode == 0
-    assert_value result == File.read!(log_filename)
-  end
-
-  test "simple diff" do
-    {result, exitcode, log_filename} =
-      prepare_and_run_test_case("simple_diff_test.exs", "n\n")
-    assert exitcode == 1
-    assert_value result == File.read!(log_filename)
-  end
-
-  test "update expected" do
-    test_source_filename = "update_expected_test.exs"
-    {result, exitcode, log_filename} = prepare_and_run_test_case(test_source_filename, "y\n")
-    assert exitcode == 0
-    assert_value result == File.read!(log_filename)
-    diff_log = "update_expected_test_diff.log" |> Path.expand(log_dir)
-    assert_value test_source_diff(test_source_filename) == File.read!(diff_log)
-  end
-
-  test "create expected" do
-    test_source_filename = "create_expected_test.exs"
-    {result, exitcode, log_filename} = prepare_and_run_test_case(test_source_filename, "y\n")
-    assert exitcode == 0
-    assert_value result == File.read!(log_filename)
-    diff_log = "create_expected_test_diff.log" |> Path.expand(log_dir)
-    assert_value test_source_diff(test_source_filename) == File.read!(diff_log)
+  
+  # these are special tests that:
+  # * launch a child mix with a .exs test file
+  # * accept or reject assert_value changes when prompted
+  # * compare both the test source file itself after this, and the
+  #   captured output of the run against stored reference copies
+  test "integration" do
+    [["simple_pass_test", "", 0],
+     ["simple_diff_test", "n\n", 1],
+     ["update_expected_test", "y\n", 0],
+     ["create_expected_test", "y\n", 0]] |>
+      Enum.each(fn([basename, responses, expected_exit_code]) ->
+        test_source_filename = basename <> ".exs"
+        {result, exitcode, log_filename} =
+          prepare_and_run_test_case(test_source_filename, responses)
+        assert exitcode == expected_exit_code
+        assert_value result == File.read!(log_filename)
+        diff_log = basename <> "_diff.log" |> Path.expand(log_dir)
+        assert_value test_source_diff(test_source_filename)
+        == File.read!(diff_log)
+      end)
   end
 
 end
