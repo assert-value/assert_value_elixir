6d054f1483f123a4c2f5f6bea0089aec5a319f56
Gleb Arshinov <gleb-oss@barsook.com>
2015-12-29 00:56:00 -0500

    Greatly simplify integration test harness.
    Changes the semantics of .after file.  This now contains the contents of
    the test after running. Prior to this it commit it contained a diff of
    after vs before test run.

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -2,28 +2,54 @@ defmodule AssertValueTest do
   use ExUnit.Case
 
   import AssertValue
-  import AssertValue.TestHelpers
-  
-  # these are special tests that:
-  # * launch a child mix with a .exs test file
+  require AssertValue.Tempfile
+
+  # these are special integration tests. For each file we:
+  # * launch a child `mix test our_integration_test.exs`
   # * accept or reject assert_value changes when prompted
-  # * compare both the test source file itself after this, and the
-  #   captured output of the run against stored reference copies
+  # * compare test source file itself after the run with a reference copy
+  # * compare test output with a reference copy
   test "integration" do
-    [["simple_pass_test", "", 0],
-     ["simple_diff_test", "n\n", 1],
-     ["update_expected_test", "y\n", 0],
-     ["create_expected_test", "y\n", 0]] |>
+    integration_test_dir = Path.expand("integration", __DIR__)
+    runnable_test_dir = AssertValue.Tempfile.mktemp_dir("assert-value-")
+
+    [["simple_pass_test.exs", "", 0],
+     ["simple_diff_test.exs", "n\n", 1],
+     ["update_expected_test.exs", "y\n", 0],
+     ["create_expected_test.exs", "y\n", 0]] |>
       Enum.each(fn([basename, responses, expected_exit_code]) ->
-        test_source_filename = basename <> ".exs"
-        {result, exitcode, log_filename} =
-          prepare_and_run_test_case(test_source_filename, responses)
+        before_filename =
+          Path.expand(basename <> ".before", integration_test_dir)
+        after_filename =
+          Path.expand(basename <> ".after", integration_test_dir)
+        output_filename =
+          Path.expand(basename <> ".output", integration_test_dir)
+
+        # copy the test to a temp dir for running
+        runnable_filename =
+          Path.expand(basename, runnable_test_dir)
+        File.cp!(before_filename, runnable_filename)
+
+        # run the test
+        %Porcelain.Result{out: output, status: exitcode} =
+          Porcelain.exec("mix", ["test", runnable_filename],
+                         in: responses)
+        # canonicalize output
+        output =
+          output
+        |> String.replace(~r{\/tmp\/assert-value-\d+-\d+-\w+/}, "")
+        |> String.replace(~r/\nFinished in.*\n/m, "")
+        |> String.replace(~r/\nRandomized with seed.*\n/m, "")
+
+        # compare the results
         assert exitcode == expected_exit_code
-        assert_value result == File.read!(log_filename)
-        diff_log = basename <> ".exs.after" |> Path.expand(integration_tests_dir)
-        assert_value test_source_diff(test_source_filename)
-        == File.read!(diff_log)
+        assert_value output
+          == File.read!(output_filename)
+        assert_value File.read!(runnable_filename)
+          == File.read!(after_filename)
       end)
   end
 
 end
+
+
