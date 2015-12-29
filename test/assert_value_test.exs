609cecb105d62c7b1aa0a62fc89d0997734fed28
Serge Smetana <smetana@pluron.com>
2015-12-30 01:37:12 +0200

    Combined all tests in one integration test.

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -8,46 +8,19 @@ defmodule AssertValueTest do
   @runnable_test_dir AssertValue.Tempfile.mktemp_dir("assert-value-")
 
   setup_all do
+    # Make sure we delete temporary dir even if tests fail
     on_exit fn -> File.rm_rf!(@runnable_test_dir) end
     :ok
   end
 
-  test "integration_test.exs" do
-    run_integration_test(__ENV__, "n\ny\ny\ny\n", 1)
-  end
-
-  test "update_file_test.exs" do
-    filename = "file_to_update"
-    file_before_path = Path.expand(filename <> ".before", @integration_test_dir)
-    file_after_path = Path.expand(filename <> ".after", @integration_test_dir)
-    file_runnable_path = Path.expand(filename, @runnable_test_dir)
-    File.cp!(file_before_path, file_runnable_path)
-    run_integration_test(__ENV__, "y\n", 0)
-    assert_value File.read!(file_runnable_path) == File.read!(file_after_path)
-  end
-
-  test "create_file_test.exs" do
-    filename = "file_to_create"
-    file_after_path = Path.expand(filename <> ".after", @integration_test_dir)
-    file_runnable_path = Path.expand(filename, @runnable_test_dir)
-    refute File.exists?(file_runnable_path)
-    run_integration_test(__ENV__, "y\n", 0)
-    assert File.exists?(file_runnable_path)
-    assert_value File.read!(file_runnable_path) == File.read!(file_after_path)
-  end
-
-  # For each test we:
-  # * launch a child `mix test our_integration_test.exs`
+  # Integration tests flow:
+  # * Copy integration_test.exs.before to @runnable_test_dir
+  # * launch a child `mix test integration_test.exs`
   # * accept or reject assert_value changes when prompted
   # * compare test source file itself after the run with a reference copy
   # * compare test output with a reference copy
-  defp run_integration_test(env, responses, expected_exit_code) do
-    # __ENV__.function returns tuple {:"test simple_pass_test.exs", 1}
-    # where first argument is a generated test function name  as atom
-    # and second argument is a test function arity (1 by default)
-    {test_name, _} = env.function
-    basename = test_name |> to_string |> String.replace(~r/^test /, "")
-
+  test "integration test" do
+    basename = "integration_test.exs"
     before_path = Path.expand(basename <> ".before", @integration_test_dir)
     after_path = Path.expand(basename <> ".after", @integration_test_dir)
     output_path = Path.expand(basename <> ".output", @integration_test_dir)
@@ -56,10 +29,23 @@ defmodule AssertValueTest do
     runnable_path = Path.expand(basename, @runnable_test_dir)
     File.cp!(before_path, runnable_path)
 
+    # Prepare data for file comparisons (create)
+    file_to_create_name = "file_to_create"
+    file_to_create_after_path = Path.expand(file_to_create_name <> ".after", @integration_test_dir)
+    file_to_create_runnable_path = Path.expand(file_to_create_name, @runnable_test_dir)
+    refute File.exists?(file_to_create_runnable_path)
+
+    # Prepare data for file comparisons (update)
+    file_to_update_name = "file_to_update"
+    file_to_update_before_path = Path.expand(file_to_update_name <> ".before", @integration_test_dir)
+    file_to_update_after_path = Path.expand(file_to_update_name <> ".after", @integration_test_dir)
+    file_to_update_runnable_path = Path.expand(file_to_update_name, @runnable_test_dir)
+    File.cp!(file_to_update_before_path, file_to_update_runnable_path)
+
     # run the test
     %Porcelain.Result{out: output, status: exitcode} =
-      Porcelain.exec("mix", ["test", "--seed", "0", runnable_path],
-                     in: responses)
+      Porcelain.exec("mix", ["test", "--seed", "0", runnable_path], in: "n\ny\ny\ny\ny\ny\n")
+
     # canonicalize output
     output =
       output
@@ -68,9 +54,12 @@ defmodule AssertValueTest do
     |> String.replace(~r/\nRandomized with seed.*\n/m, "")
 
     # compare the results
-    assert exitcode == expected_exit_code
+    assert exitcode == 1 # One fail
     assert_value output == File.read!(output_path)
     assert_value File.read!(runnable_path) == File.read!(after_path)
+
+    assert_value File.read!(file_to_create_runnable_path) == File.read!(file_to_create_after_path)
+    assert_value File.read!(file_to_update_runnable_path) == File.read!(file_to_update_after_path)
   end
 
 end
