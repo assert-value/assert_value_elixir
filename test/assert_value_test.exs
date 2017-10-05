02ac3e78c3e989001cef78950b1a76547aa6c60d
Serge Smetana <smetana@pluron.com>
2017-10-05 18:41:01 +0300

    Reduced code duplication in tests: Implemented AssertValue.IntegrationTest module with integration_test macro

--- test/assert_value_test.exs
+++ test/assert_value_test.exs
@@ -2,100 +2,40 @@ defmodule AssertValueTest do
   use ExUnit.Case
 
   import AssertValue
-  require AssertValue.Tempfile
-
-  @integration_test_dir Path.expand("integration", __DIR__)
-  @runnable_test_dir AssertValue.Tempfile.mktemp_dir("assert-value-")
+  require AssertValue.IntegrationTest
+  import AssertValue.IntegrationTest
 
   setup_all do
     # Make sure we delete temporary dir even if tests fail
-    on_exit fn -> File.rm_rf!(@runnable_test_dir) end
+    on_exit fn ->
+      File.rm_rf!(AssertValue.IntegrationTest.runnable_test_dir)
+    end
     :ok
   end
 
-  defp prepare_runnable_test(basename) do
-    before_path = Path.expand(basename <> ".before", @integration_test_dir)
-    after_path = Path.expand(basename <> ".after", @integration_test_dir)
-    output_path = Path.expand(basename <> ".output", @integration_test_dir)
-
-    # copy the test to a temp dir for running
-    runnable_path = Path.expand(basename, @runnable_test_dir)
-    File.cp!(before_path, runnable_path)
-    {runnable_path, after_path, output_path}
-  end
-
-  defp run_tests(filename) do
-    # extract expected assert_value prompt responses from the test.
-    # We look for lines like '# prompt: y'
-    prompt_responses =
-      Regex.scan(~r/#\s*prompt:\s*(.)/, File.read!(filename))
-      |> Enum.map(fn([_, x]) -> x end)
-      |> Enum.join("\n")
-      |> Kernel.<>("\n")
-
-    {output, exitcode} = AssertValue.System.exec("mix",
-      ["test", "--seed", "0", filename], input: prompt_responses)
-
-    # Canonicalize output
-    output =
-      output
-      |> String.replace(~r{\/tmp\/assert-value-\d+-\d+-\w+/}, "")
-      |> String.replace(~r/\nFinished in.*\n/m, "")
-      |> String.replace(~r/\nRandomized with seed.*\n/m, "")
-      # canonicalize ExUnit error formatting:
-      # - remove fancy spacing
-      # - canonicalize lhs/rhs vs left/right
-      |> String.replace(~r/\s{5}code:\s+actual/m,   "     code: actual")
-      |> String.replace(~r/\s{5}(lhs|left):\s+"/m,  "     left: \"")
-      |> String.replace(~r/\s{5}(rhs|right):\s+"/m, "     right: \"")
-      # canonicalize messages about raised AssertValue.ArgumentError exceptions
-      # ExUnit in Elixir 1.5 has "code:" line in message:
-      #
-      #   ** (AssertValue.ArgumentError) ...
-      #   code: assert_value "foo" = "bar"
-      #   stacktrace:
-      #     integration_test.exs:82: (test)
-      #
-      # ExUnit in Elixir 1.4 does not
-      #
-      #   ** (AssertValue.ArgumentError) ...
-      #   stacktrace:
-      #     integration_test.exs:82: (test)
-      #
-      |> String.replace(
-        ~r/(\(AssertValue.ArgumentError\).*?)\n\s{5}code:.*?\n/, "\\1\n")
-
-    {output, exitcode}
-  end
-
-  # Integration tests flow:
-  # * Copy integration_test.exs.before to @runnable_test_dir
-  # * launch a child `mix test integration_test.exs`
-  # * accept or reject assert_value changes when prompted
-  # * compare test source file itself after the run with a reference copy
-  # * compare test output with a reference copy
-
+  # This test is different from the rest because we test here also
+  # for file create/update
   test "integration" do
-    {runnable_path, after_path, output_path} =
-      prepare_runnable_test("integration_test.exs")
-
     # Prepare data for file comparisons (create)
     file_to_create_name = "file_to_create"
     file_to_create_after_path = Path.expand(file_to_create_name <> ".after",
-      @integration_test_dir)
+      AssertValue.IntegrationTest.integration_test_dir)
     file_to_create_runnable_path = Path.expand(file_to_create_name,
-      @runnable_test_dir)
+      AssertValue.IntegrationTest.runnable_test_dir)
     refute File.exists?(file_to_create_runnable_path)
 
     # Prepare data for file comparisons (update)
     file_to_update_name = "file_to_update"
     file_to_update_before_path = Path.expand(file_to_update_name <> ".before",
-      @integration_test_dir)
+      AssertValue.IntegrationTest.integration_test_dir)
     file_to_update_after_path = Path.expand(file_to_update_name <> ".after",
-      @integration_test_dir)
+      AssertValue.IntegrationTest.integration_test_dir)
     file_to_update_runnable_path = Path.expand(file_to_update_name,
-      @runnable_test_dir)
+      AssertValue.IntegrationTest.runnable_test_dir)
     File.cp!(file_to_update_before_path, file_to_update_runnable_path)
+
+    {runnable_path, after_path, output_path} =
+      prepare_runnable_test("integration_test.exs")
 
     {output, exitcode} = run_tests(runnable_path)
     assert exitcode == 1 # There were failed tests
@@ -104,54 +44,23 @@ defmodule AssertValueTest do
     assert_value File.read!(runnable_path) == File.read!(after_path)
     assert_value output == File.read!(output_path)
 
+    # test created/updated files
     assert_value File.read!(file_to_create_runnable_path) ==
       File.read!(file_to_create_after_path)
     assert_value File.read!(file_to_update_runnable_path) ==
       File.read!(file_to_update_after_path)
   end
 
-  test "diff and help prompt" do
-    {runnable_path, after_path, output_path} =
-      prepare_runnable_test("diff_and_help_prompt_test.exs")
+  integration_test "diff and help promt", "diff_and_help_prompt_test.exs",
+    exitcode: 1 # There should be failed tests
 
-    {output, exitcode} = run_tests(runnable_path)
-    assert exitcode == 1 # There were failed tests
+  integration_test "accept all (Y)", "accept_all_test.exs",
+    exitcode: 0 # No failed tests
 
-    assert_value File.read!(runnable_path) == File.read!(after_path)
-    assert_value output == File.read!(output_path)
-  end
+  integration_test "accept all with errors (Y)", "accept_all_with_error_test.exs",
+    exitcode: 1
 
-  test "accept all (Y)" do
-    {runnable_path, after_path, output_path} =
-      prepare_runnable_test("accept_all_test.exs")
-
-    {output, exitcode} = run_tests(runnable_path)
-    assert exitcode == 0 # All tests pass
-
-    assert_value File.read!(runnable_path) == File.read!(after_path)
-    assert_value output == File.read!(output_path)
-  end
-
-  test "accept all with error (Y)" do
-    {runnable_path, after_path, output_path} =
-      prepare_runnable_test("accept_all_with_error_test.exs")
-
-    {output, exitcode} = run_tests(runnable_path)
-    assert exitcode == 1 # There were failed tests
-
-    assert_value File.read!(runnable_path) == File.read!(after_path)
-    assert_value output == File.read!(output_path)
-  end
-
-  test "decline all (N)" do
-    {runnable_path, after_path, output_path} =
-      prepare_runnable_test("decline_all_test.exs")
-
-    {output, exitcode} = run_tests(runnable_path)
-    assert exitcode == 1 # There were failed tests
-
-    assert_value File.read!(runnable_path) == File.read!(after_path)
-    assert_value output == File.read!(output_path)
-  end
+  integration_test "decline(Y)", "decline_all_test.exs",
+    exitcode: 1
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -54,3 +54,102 @@ defmodule AssertValue.System do
     end
   end
 end
+
+defmodule AssertValue.IntegrationTest do
+
+  require AssertValue.Tempfile
+
+  @integration_test_dir Path.expand("integration", __DIR__)
+  @runnable_test_dir AssertValue.Tempfile.mktemp_dir("assert-value-")
+
+  def integration_test_dir do
+    @integration_test_dir
+  end
+
+  def runnable_test_dir do
+    @runnable_test_dir
+  end
+
+  def prepare_runnable_test(basename) do
+    before_path = Path.expand(basename <> ".before", @integration_test_dir)
+    after_path = Path.expand(basename <> ".after", @integration_test_dir)
+    output_path = Path.expand(basename <> ".output", @integration_test_dir)
+
+    # copy the test to a temp dir for running
+    runnable_path = Path.expand(basename, @runnable_test_dir)
+    File.cp!(before_path, runnable_path)
+    {runnable_path, after_path, output_path}
+  end
+
+  def run_tests(filename) do
+    # extract expected assert_value prompt responses from the test.
+    # We look for lines like '# prompt: y'
+    prompt_responses =
+      Regex.scan(~r/#\s*prompt:\s*(.)/, File.read!(filename))
+      |> Enum.map(fn([_, x]) -> x end)
+      |> Enum.join("\n")
+      |> Kernel.<>("\n")
+
+    {output, exitcode} = AssertValue.System.exec("mix",
+      ["test", "--seed", "0", filename], input: prompt_responses)
+
+    # Canonicalize output
+    output =
+      output
+      |> String.replace(~r{\/tmp\/assert-value-\d+-\d+-\w+/}, "")
+      |> String.replace(~r/\nFinished in.*\n/m, "")
+      |> String.replace(~r/\nRandomized with seed.*\n/m, "")
+      # canonicalize ExUnit error formatting:
+      # - remove fancy spacing
+      # - canonicalize lhs/rhs vs left/right
+      |> String.replace(~r/\s{5}code:\s+actual/m,   "     code: actual")
+      |> String.replace(~r/\s{5}(lhs|left):\s+"/m,  "     left: \"")
+      |> String.replace(~r/\s{5}(rhs|right):\s+"/m, "     right: \"")
+      # canonicalize messages about raised AssertValue.ArgumentError exceptions
+      # ExUnit in Elixir 1.5 has "code:" line in message:
+      #
+      #   ** (AssertValue.ArgumentError) ...
+      #   code: assert_value "foo" = "bar"
+      #   stacktrace:
+      #     integration_test.exs:82: (test)
+      #
+      # ExUnit in Elixir 1.4 does not
+      #
+      #   ** (AssertValue.ArgumentError) ...
+      #   stacktrace:
+      #     integration_test.exs:82: (test)
+      #
+      |> String.replace(
+        ~r/(\(AssertValue.ArgumentError\).*?)\n\s{5}code:.*?\n/, "\\1\n")
+
+    {output, exitcode}
+  end
+
+  # Integration tests flow:
+  # * Copy integration_test.exs.before to @runnable_test_dir
+  # * launch a child `mix test integration_test.exs`
+  # * accept or reject assert_value changes when prompted
+  # * compare test source file itself after the run with a reference copy
+  # * compare test output with a reference copy
+
+  # integration_test "accept all (Y)", "accept_all_test.exs", exitcode: 1
+  defmacro integration_test(test_name, test_filename, opts \\ []) do
+    quote do
+      test unquote(test_name) do
+        {runnable_path, after_path, output_path} =
+          prepare_runnable_test(unquote(test_filename))
+
+        {output, exitcode} = run_tests(runnable_path)
+        assert exitcode == unquote(exitcode(opts))
+
+        assert_value File.read!(runnable_path) == File.read!(after_path)
+        assert_value output == File.read!(output_path)
+      end
+    end
+  end
+
+  defp exitcode(opts) do
+    opts[:exitcode] || 0
+  end
+
+end
