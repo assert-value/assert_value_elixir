91cfeeab90e73efa73a761cf18e614c0b81c7d32
Serge Smetana <smetana@650health.com>
2022-05-10 20:41:23 +0300

    Add workaround for Code.string_to_quoted error in Elixir 1.13
    https://github.com/elixir-lang/elixir/issues/11813

--- lib/assert_value/parser.ex
+++ lib/assert_value/parser.ex
@@ -157,16 +157,22 @@ defmodule AssertValue.Parser do
   # Empty code does not match anything
   defp code_match_ast?("", _ast), do: false
   defp code_match_ast?(code, ast) do
-    case Code.string_to_quoted(code) do
-      {:ok, quoted} ->
-        quoted = if is_binary(quoted) do
-          String.replace(quoted, "<NOEOL>\\n", "")
-        else
-          quoted
-        end
-        ast_match?(quoted, ast)
-      _ ->
-        false
+    # Elixir 1.13 raises MatchError on some escape characters
+    # https://github.com/elixir-lang/elixir/issues/11813
+    try do
+      case Code.string_to_quoted(code) do
+        {:ok, quoted} ->
+          quoted = if is_binary(quoted) do
+            String.replace(quoted, "<NOEOL>\\n", "")
+          else
+            quoted
+          end
+          ast_match?(quoted, ast)
+        _ ->
+          false
+      end
+    rescue
+      _ -> false
     end
   end
 
