2d47b0f28bd052af9bfd4c408ddca9a99d963152
Serge Smetana <smetana@650health.com>
2026-07-02 19:42:02 +0300

    Fix flaky integration tests. Reduce async test output interference
    Sleep before entering the AssertValue server instead of inside it.
    
    This gives pending ExUnit output a chance to flush before the next prompt is
    printed, avoiding flaky output ordering between async tests.

--- lib/assert_value/server.ex
+++ lib/assert_value/server.ex
@@ -75,11 +75,6 @@ defmodule AssertValue.Server do
   end
 
   def handle_call({:ask_user_about_diff, opts}, _from, state) do
-    # Hack: We try to wait until previous test asking about diff (and fail) will
-    # output test results. Otherwise user will get previous failed test result
-    # message right after the answer for current test.
-    # TODO: Refactor to messaging
-    Process.sleep(30)
     contents = StringIO.flush(state.captured_ex_unit_io_pid)
     if contents != "", do: IO.write(contents)
 
@@ -137,6 +132,11 @@ defmodule AssertValue.Server do
   end
 
   def ask_user_about_diff(opts) do
+    # Hack: We try to wait until previous test asking about diff (and fail) will
+    # output test results. Otherwise user will get previous failed test result
+    # message right after the answer for current test.
+    # TODO: Refactor to messaging
+    Process.sleep(30)
     GenServer.call(__MODULE__, {:ask_user_about_diff, opts}, :infinity)
   end
 
